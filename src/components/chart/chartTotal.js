c17ab2a34653f3f989470e56f36a07282b805f66
OzanCanAkn <ozancanakn@gmail.com>
2021-12-09 16:44:35 +0300

    for displayin 0.00 when chart is empty

--- src/components/chart/chartTotal.js
+++ src/components/chart/chartTotal.js
@@ -1,6 +1,5 @@
 import React from "react";
-export default function ChartTotal({total}) {
-  
+export default function ChartTotal({ total }) {
   return (
     <div
       style={{
@@ -8,8 +7,8 @@ export default function ChartTotal({total}) {
         color: "#1EA4CE",
         width: "fit-content",
         height: "fit-content",
-        marginTop:10,
-        padding:"20px 30px",
+        marginTop: 10,
+        padding: "20px 30px",
         display: "flex",
         alignItems: "center",
         border: "solid 3px #1EA4CE",
@@ -17,7 +16,7 @@ export default function ChartTotal({total}) {
       item
     >
       <p style={{ color: "#1EA4CE", textAlign: "center", margin: "auto" }}>
-      ₺ {total}
+        ₺ {`${total ? total : "0.00"}`}
       </p>
     </div>
   );
