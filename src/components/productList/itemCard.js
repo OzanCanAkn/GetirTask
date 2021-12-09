a5fd2673e91edd90714118311683cff9e3771ef9
OzanCanAkn <ozancanakn@gmail.com>
2021-12-09 11:18:28 +0300

    item card align problem fixed

--- src/components/productList/itemCard.js
+++ src/components/productList/itemCard.js
@@ -13,19 +13,19 @@ const ColorButton = styled(Button)(() => ({
   textTransform: "capitalize",
   height: 22,
 }));
-export default function ItemCard({ item}) {
-  //props are price, image and name attributes of initial item
+export default function ItemCard({ item }) {
+  //prop is a item from items.json
   return (
     <Grid
       item
       xs={6}
       md={4}
       lg={3}
-      container
       direction="column"
-      justifyContent="flex-start"
     >
       <div
+        item
+        xs="6"
         style={{
           padding: 16,
           border: "2px solid #f3f0fe",
@@ -39,32 +39,43 @@ export default function ItemCard({ item}) {
           src={placeHolder}
         ></img>
       </div>
-      <p
-        style={{
-          fontFamily: "Helvetica",
-          fontStyle: "normal",
-          fontWeight: "bold",
-          fontSize: 14,
-          color: "#1EA4CE",
-          margin:8
-        }}
+      <Grid
+        xs="6"
+        item
+        container
+        direction="column"
+        style={{ height: "100%",flex:"1" }}
+        alignItems="space-between"
+        justifyContent="space-between"
       >
-        ₺ {item.price}
-      </p>
-      <p
-        style={{
-          fontFamily: "Open Sans",
-          fontStyle: "normal",
-          fontWeight: 600,
-          fontSize: 14,
-          lineHeight: 1.6,
-          color: " #191919",
-          margin:7
-        }}
-      >
-        {item.name}
-      </p>
-      <ColorButton style={{marginTop:"auto"}} variant="contained">Add</ColorButton>
+        <p
+          style={{
+            fontFamily: "Helvetica",
+            fontStyle: "normal",
+            fontWeight: "bold",
+            fontSize: 14,
+            color: "#1EA4CE",
+            margin: 8,
+          }}
+        >
+          ₺ {item.price}
+        </p>
+        <p
+          style={{
+            fontFamily: "Open Sans",
+            fontStyle: "normal",
+            fontWeight: 600,
+            fontSize: 14,
+            lineHeight: 1.6,
+            color: " #191919",
+            margin: 7,
+            minHeight:"3rem"
+          }}
+        >
+          {item.name}
+        </p>
+        <ColorButton variant="contained">Add</ColorButton>
+      </Grid>
     </Grid>
   );
 }
