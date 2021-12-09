6862a37519f9544695b4588113b299fd4867c8bf
OzanCanAkn <ozancanakn@gmail.com>
2021-12-09 16:51:22 +0300

    header basket value connected to redux

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,23 @@
 import "./header.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { ReactComponent as logo } from "../../assets/svg/logo.svg";
 import { ReactComponent as lock } from "../../assets/svg/lock.svg";
 import { Grid } from "@mui/material";
 import SvgIcon from "@mui/material/SvgIcon";
+
 function Basket() {
+  const items = useSelector((state) => state.chartReducer);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    let addition = 0;
+    items.map((item) => {
+      addition = addition + item.price * item.count;
+    });
+    setTotal(Math.round(addition * 100) / 100);
+    console.log(items);
+  }, [items]);
+
   return (
     <Grid
       direction="row"
@@ -16,7 +29,7 @@ function Basket() {
       xs={3}
     >
       <SvgIcon id="lock" viewBox="0 0 14 18" component={lock} />
-      <b style={{ color: "white" }}>₺ 15.45</b>
+      <b style={{ color: "white" }}>₺ {`${total ? total : "0.00"}`}</b>
     </Grid>
   );
 }
@@ -25,11 +38,16 @@ export default function Header() {
   return (
     <Grid id="container">
       <SvgIcon
-        style={{ width: "142", height: "7vh" ,position:"absolute"}}
+        style={{ width: "142", height: "7vh", position: "absolute" }}
         viewBox="0 0 142 41"
         component={logo}
       />
-      <Grid columns={24} alignItems="center" justifyContent="flex-end" container>
+      <Grid
+        columns={24}
+        alignItems="center"
+        justifyContent="flex-end"
+        container
+      >
         <Basket></Basket>
       </Grid>
     </Grid>
