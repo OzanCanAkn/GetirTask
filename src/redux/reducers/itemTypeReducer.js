1212643b231660e7bfbbf0d4ec1b878502d8a75a
OzanCanAkn <ozancanakn@gmail.com>
2021-12-11 11:14:46 +0300

    console.logs deleted and removed unused codes

--- src/redux/reducers/itemTypeReducer.js
+++ src/redux/reducers/itemTypeReducer.js
@@ -1,14 +1,14 @@
 import * as actionTypes from "../actions/actionTypes";
 
 const itemTypeReducer = (state = "mug", actions) => {
-  let newState;
   switch (actions.type) {
     case actionTypes.CHANGE_ITEM_TYPE:
       if (state === "mug") {
-        return (newState = "shirt");
+        return "shirt";
       } else if (state === "shirt") {
-        return (newState = "mug");
+        return "mug";
       }
+      break;
     default:
       return state;
   }
