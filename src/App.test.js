39468122a9dc1a9c258dae63532a0eab7e85a160
OzanCanAkn <ozancanakn@gmail.com>
2021-12-12 13:35:30 +0300

    tests added

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,69 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import App from "./App";
+import { shallow } from "enzyme";
+import configureStore from "./redux/reducers/configureStore";
+const store = configureStore();
+import {
+  ProductList,
+  Header,
+  CheckboxGroup,
+  SortTab,
+  Chart,
+  Footer,
+  BrandsCheckbox,
+} from "./components";
+import ChartTotal from "./components/chart/chartTotal";
+describe("rendering components", () => {
+  it("renders ProductList component without crash", () => {
+    shallow(
+      <Provider store={store}>
+        <ProductList />
+      </Provider>
+    );
+  });
+  it("renders CheckboxGroup component without crash", () => {
+    shallow(
+      <Provider store={store}>
+        <CheckboxGroup />
+      </Provider>
+    );
+  });
+  it("renders SortTab component without crash", () => {
+    shallow(
+      <Provider store={store}>
+        <SortTab />
+      </Provider>
+    );
+  });
+  it("renders Footer component without crash", () => {
+    shallow(
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    );
+  });
+  it("renders Header component without crash", () => {
+    shallow(
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    );
+  });
+  it("renders Chart component without crash", () => {
+    shallow(
+      <Provider store={store}>
+        <Chart />
+      </Provider>
+    );
+  });
+  it("renders BrandsCheckbox component without crash", () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <BrandsCheckbox />
+      </Provider>
+    );
+    expect(wrapper);
+  });
 });
